a4b2960ac333e74e66d304aceeced526d04fd813
Eduardo Sanches Bocato <dubocato@gmail.com.br>
2018-05-25 07:16:34 -0300

    Changes

--- PokeApp/Scenes/Favorites/ViewControllers/FavoritesViewController.swift
+++ PokeApp/Scenes/Favorites/ViewControllers/FavoritesViewController.swift
@@ -7,29 +7,48 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class FavoritesViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
+    // MARK: - Dependencies
+    let viewModel = FavoritesViewModel()
+    let disposeBag = DisposeBag()
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        bindAll()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: -
+
+}
+
+// MARK: - Binding
+private extension FavoritesViewController {
+    
+    
+    func bindAll() {
+        bindViewModel()
+        bindCollectionView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func bindViewModel() {
+        
     }
-    */
-
+    
+    func bindCollectionView() {
+        
+        
+        
+    }
+    
 }
--- PokeApp/Scenes/Favorites/ViewModels/FavoritesViewModel.swift
+++ PokeApp/Scenes/Favorites/ViewModels/FavoritesViewModel.swift
@@ -7,7 +7,26 @@
 //
 
 import Foundation
+import RxSwift
 
 class FavoritesViewModel {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - RXProperties
+    var favoritesCellModels = Variable<[FavoriteCollectionViewCellModel]?>([])
+    
+    // MARK: - Initialzation
+    init() {
+        loadFavorites()
+    }
+    
+    // MARK: -
+    private func loadFavorites() { // TODO: Refactor
+        favoritesCellModels.value = FavoritesManager.shared.favorites.map({ (pokemon) -> FavoriteCollectionViewCellModel in
+            return FavoriteCollectionViewCellModel(data: pokemon)
+        })
+    }
     
 }
