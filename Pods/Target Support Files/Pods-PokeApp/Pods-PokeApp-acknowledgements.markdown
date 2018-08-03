af86d119cb5e9f4b2c178d0d4469ccd6d078e219
Eduardo Sanches Bocato <dubocato@gmail.com>
2018-08-03 19:37:30 -0300

    Adding shared scheme

--- Pods/Target Support Files/Pods-PokeApp/Pods-PokeApp-acknowledgements.markdown
+++ Pods/Target Support Files/Pods-PokeApp/Pods-PokeApp-acknowledgements.markdown
@@ -227,7 +227,11 @@ Copyright 2018 Google
 
 Copyright 2018 Google
 
-## GoogleToolboxForMac
+## GoogleAppMeasurement
+
+Copyright 2018 Google
+
+## GoogleUtilities
 
 
                                  Apache License
--- Pods/Target Support Files/Pods-PokeApp/Pods-PokeApp-frameworks.sh
+++ Pods/Target Support Files/Pods-PokeApp/Pods-PokeApp-frameworks.sh
@@ -143,7 +143,7 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
@@ -151,7 +151,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
